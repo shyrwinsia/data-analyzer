db83564660e3899f5d943be73a6a902ebd1bdbee
Shyrwinsteel Sia <shyrwin69@gmail.com>
2020-08-31 13:11:27 +0800

    Change URLs

--- map/src/js/index.js
+++ map/src/js/index.js
@@ -1,17 +1,17 @@
 var promises = [
   d3.json('https://d3js.org/us-10m.v1.json'),
   d3
-    .json('https://api.jsonbin.io/b/5f4c79f74d8ce4111385197d')
+    .json('https://api.jsonbin.io/b/5f4c8259993a2e110d3ad302/latest')
     .then(function (d) {
       console.log(d);
     }),
   d3
-    .json('https://api.jsonbin.io/b/5f4c81c2514ec5112d12aa06')
+    .json('https://api.jsonbin.io/b/5f4c81c2514ec5112d12aa06/latest')
     .then(function (d) {
       console.log(d);
     }),
   d3
-    .json('https://api.jsonbin.io/b/5f4c79f74d8ce4111385197d')
+    .json('https://api.jsonbin.io/b/5f4c79f74d8ce4111385197d/latest')
     .then(function (d) {
       console.log(d);
     })
