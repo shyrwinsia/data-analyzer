d6b58433cc9d6116b682454b198b3d4283deccbe
Shyrwinsteel Sia <shyrwin69@gmail.com>
2020-08-31 13:08:38 +0800

    Changed US topology

--- map/src/js/index.js
+++ map/src/js/index.js
@@ -1,7 +1,5 @@
 var promises = [
-  d3.json(
-    'https://raw.githubusercontent.com/shyrwinsia/covid-ph-data/master/ph-topology.json'
-  ),
+  d3.json('https://d3js.org/us-10m.v1.json'),
   d3
     .json('https://api.jsonbin.io/b/5f4c79f74d8ce4111385197d')
     .then(function (d) {
